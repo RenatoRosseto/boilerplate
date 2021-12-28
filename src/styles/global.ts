6ccb5a19b2922dac6a511659c7bd401d8b9ebe3f
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-12-28 18:32:06 -0300

    style: adding project fonts and theme tokens

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,23 +1,72 @@
-import { createGlobalStyle } from 'styled-components';
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent,
+} from 'styled-components';
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean;
+};
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 300;
+    font-display: swap;
+    src: local('Roboto Light'), local('Roboto-Light'),
+        url('/fonts/roboto-v29-latin-300.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: local('Roboto Regular'), local('Roboto-Regular'),
+        url('/fonts/roboto-v29-latin-regular.woff2') format('woff2');
+  }
+
+  @font-face {
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 700;
+    font-display: swap;
+    src: local('Roboto SemiBold'), local('Roboto-SemiBold'),
+        url('/fonts/roboto-v29-latin-700.woff2') format('woff2'),;
+  }
+
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-  }
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 
-  html {
-    font-size: 62.5%;
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
   }
+  ${({ theme, removeBg }) => css`
+    html {
+      font-size: 62.5%;
+    }
 
-  html, body, #__next {
-    height: 100%;
-  }
-
-  body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
-  }
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
+    }
+  `}
 `;
 
 export default GlobalStyles;
