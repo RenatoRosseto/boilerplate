42ecb7e847552a6cc50b57b70813010c56cf05d2
Renato Rosseto <renato_rosseto_neto@hotmail.com>
2021-12-30 20:02:56 -0300

    chore: adding example with type in type.ts file

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,5 +1,11 @@
 import React from 'react';
 
+// export type ButtonProps = {
+//   primary?: boolean;
+//   size?: 'small' | 'medium' | 'large';
+//   fullWidth?: boolean;
+//   id?: string;
+// };
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     React.AriaAttributes {
